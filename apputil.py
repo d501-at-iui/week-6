243f0ad04496f714858b55d81b240fc4ef42cbbb
Danielle D. <dani.dixonpro@gmail.com>
2026-02-25 17:37:29 -0500

    Ex. 2 API requests, we left off obtaining the API key for Genius

--- apputil.py
+++ apputil.py
@@ -1,6 +1,7 @@
 # your code here ...
 
 from apputil import Genius
+import json
 
 # Exercise 1
 genius = Genius(access_token = "access_token")
@@ -11,4 +12,17 @@ Python class Genius initialize object, saves access token as an attribute.
 class Genius:
     def __init__ (self, access_token):
         self.access_token = access_token
-# Comment
+#Exercise 2
+    search_term = 'Audioslave'
+    genius.get_artist(search_term)
+    """Extract most likely 'Primary' Artist_ID from the first 'hit'
+        of the search
+     Use API path under 'Artists' for this Artist_ID to pull info. about artist
+    """
+    genius_search_url = f"http://api.genius.com/search?=
+                        {search_term}&access_token={ACCESS_TOKEN}"
+    response = requests.get(genius_search_url)
+    json_data = response.json()
+    
+
+
